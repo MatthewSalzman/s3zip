611209c7a8bf28707da83a75ea9991390643e7bd
Matthew Salzman <salzmamj@mail.uc.edu>
2020-10-18 19:13:36 -0400

    Added tar support

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"archive/tar"
 	"archive/zip"
+	"compress/gzip"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -24,6 +26,9 @@ import (
 var config = Configuration{}
 var s3Svc *s3.S3
 var s3downloader *s3manager.Downloader
+var prefix string
+var writePath string
+var compType string
 
 type Configuration struct {
 	AccessKey string
@@ -80,11 +85,19 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-
+	compType = "zip"
 	health, ok := r.URL.Query()["health"]
 	if len(health) > 0 {
 		fmt.Fprintf(w, "OK")
 		return
+	}
+
+	cType, ok := r.URL.Query()["comp"]
+	if len(cType) > 0 {
+		fmt.Println("comp tpye", cType)
+		if cType[0] == "tar" {
+			compType = "tar"
+		}
 	}
 
 	// Get "prefix" URL params
@@ -93,12 +106,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Prefix not specified. Pass ?prefix= to use.", 500)
 		return
 	}
-	prefix := pres[0]
+	prefix = pres[0]
 
 	// Get path url parms
 	// Specifys the folder name they want to write to in the zip
 	path, ok := r.URL.Query()["path"]
-	var writePath string
+
 	if !ok || len(pres) < 1 {
 		// if no path is specified write to root folder
 		writePath = "/"
@@ -115,17 +128,67 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		Bucket: aws.String(config.Bucket),
 		Prefix: aws.String(prefix),
 	})
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	// Start processing the response
-	w.Header().Add("Content-Disposition", "attachment; filename=\""+GetName(prefix)+".zip"+"\"")
-	w.Header().Add("Content-Type", "application/zip")
+	w.Header().Add("Content-Disposition", "attachment; filename=\""+GetName(prefix)+"."+compType+"\"")
+	w.Header().Add("Content-Type", "application/"+compType)
+
+	// tarit(res.Contents)
+	if compType == "zip" {
+		zipit(w, res.Contents)
+	}
+	if compType == "tar" {
+		tarit(w, res.Contents)
+	}
 
 	// Loop over files, add them to the
+
+	log.Printf("%s\t%s\t%s", compType, r.RequestURI, time.Since(start))
+}
+
+func tarit(w http.ResponseWriter, contents []*s3.Object) {
+	gw := gzip.NewWriter(w)
+	defer gw.Close()
+	tw := tar.NewWriter(gw)
+	defer tw.Close()
+	for _, file := range contents {
+		fmt.Println("File", *file)
+		filePath := *file.Key
+		path := strings.Replace(filePath, prefix, writePath, 1)
+
+		header := new(tar.Header)
+		header.Name = path
+		header.Size = *file.Size
+		// header.Mode = int64(0755) // TODO?
+		header.ModTime = *file.LastModified
+		// write the header to the tarball archive
+		if err := tw.WriteHeader(header); err != nil {
+			// return err;;
+			fmt.Println("err", err)
+		}
+
+		// Read file from S3, log any errors
+		_, err := s3downloader.Download(FakeWriterAt{tw}, &s3.GetObjectInput{
+			Bucket: aws.String(config.Bucket),
+			Key:    aws.String(*file.Key),
+		})
+		if err != nil {
+			fmt.Println("dl err", err)
+		}
+
+	}
+	return
+}
+
+func zipit(w http.ResponseWriter, contents []*s3.Object) {
 	zipWriter := zip.NewWriter(w)
 	// pr, pw := io.Pipe()
 	// Create zip.Write which will writes to pipes
 
-	for _, file := range res.Contents {
+	for _, file := range contents {
 		fmt.Println("File", *file)
 
 		// safeFileName := makeSafeFileName.ReplaceAllString(*file.Key, "")
@@ -154,7 +217,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		// Build safe file file name
 
 		// Read file from S3, log any errors
-		_, err = s3downloader.Download(FakeWriterAt{f}, &s3.GetObjectInput{
+		_, err := s3downloader.Download(FakeWriterAt{f}, &s3.GetObjectInput{
 			Bucket: aws.String(config.Bucket),
 			Key:    aws.String(*file.Key),
 		})
@@ -165,8 +228,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	zipWriter.Close()
-
-	log.Printf("%s\t%s\t%s", r.Method, r.RequestURI, time.Since(start))
+	return
 }
 
 var makeSafeFileName = regexp.MustCompile(`[#<>:"/\|?*\\]`)
